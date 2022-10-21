ae5b0f098ea96d663f34f391e5a07fefc9096e5f
a578ff6 <116147933+a578ff6@users.noreply.github.com>
2022-10-22 04:01:31 +0800

    Add files via upload

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by 曹家瑋 on 2022/6/9.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by 曹家瑋 on 2022/6/9.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let htmlPath = Bundle.main.path(forResource: "BullsEye", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
